465624c1762398e66ea3dc89d853be239239b03a
karengoncalves8 <karengoncalves128@gmail.com>
2023-08-04 11:30:48 -0300

    att + agenda

--- LibellaApp/pages/home/Agenda.js
+++ LibellaApp/pages/home/Agenda.js
@@ -1,5 +1,4 @@
-import * as React from "react";
-import { StatusBar } from "expo-status-bar";
+import React, {useState} from 'react';
 import {
   StyleSheet,
   Text,
@@ -11,7 +10,7 @@ import {
 import { LinearGradient } from "expo-linear-gradient"; /* instalar */
 import TabContainer from "../../components/navigation/TabContainer";
 
-import {Calendar, LocaleConfig} from 'react-native-calendars';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
 
 const meetings = [
   {
@@ -22,49 +21,62 @@ const meetings = [
     startDatetime: '2022-05-11T13:00',
     endDatetime: '2022-05-11T14:30',
   },
+  {
+    id: 2,
+    name: 'Alo',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    startDatetime: '2022-05-11T13:00',
+    endDatetime: '2022-05-11T14:30',
+  },
 ]
 
-const renderMettings = meetings.map((item, index) => 
-                             <View key={index}>{item}</View>
-                           );
+
+
+
+
 const AgendaPage = () => {
+  const [selected, setSelected] = useState('');
+
   return (
     <TabContainer>
       <View style={styles.container}>
-      <Calendar
-  // Customize the appearance of the calendar
-  style={{
-    width: '100%'
-  }}
-  // Specify the current date
-  current={'2023-08-01'}
-  // Callback that gets called when the user selects a day
-  onDayPress={day => {
-    console.log('selected day', day);
-  }}
-  // Mark specific dates as marked
-  markedDates={{
-    '2012-03-01': {selected: true, marked: true, selectedColor: 'blue'},
-    '2012-03-02': {marked: true},
-    '2012-03-03': {selected: true, marked: true, selectedColor: 'blue'}
-  }}
-/>
+        <Calendar
+          // Customize the appearance of the calendar
+          style={{
+            width: '100%'
+          }}
+          // Callback that gets called when the user selects a day
+          onDayPress={day => {
+            setSelected(day.dateString);
+          }}
+          // Mark specific dates as marked
+          markedDates={{
+            '2012-03-01': { selected: true, marked: true, selectedColor: 'blue' },
+            '2012-03-02': { marked: true },
+            '2012-03-03': { selected: true, marked: true, selectedColor: 'blue' }
+          }}
+        />
 
-  <View style={{paddingHorizontal: 30, flex: 1, alignItems: 'center'}}>
-    <View style={{width:'100%'}}>
-      <Text>Alo</Text>
-      <View style={styles.card}>
-        {meetings.map((meetings) => {
-        return (
-          <View>
-            <Text>{meetings.name}</Text>
+        <View style={{ paddingHorizontal: 30, flex: 1, alignItems: 'center' }}>
+          <View style={{ width: '100%' }}>
+            <View style={{gap: 30}}>
+            <Text>{selected}</Text>
+              {meetings.map((mettings, i) => {
+                return (
+                  <View key={i}>
+                    <Text >{mettings.startDatetime}</Text>
+                    <View style={styles.card}>
+                    <Text>  {mettings.name}</Text>
+                    </View>
+                  </View>
+                )
+              })}
+              
+            </View>
           </View>
-        );
-      })}
-      </View>
-    </View>
 
-  </View>
+        </View>
       </View>
     </TabContainer>
   );
