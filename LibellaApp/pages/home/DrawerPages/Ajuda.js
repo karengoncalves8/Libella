7055b477d8c246fbce5b883562823d9e93d63265
ruanpreboucas <ruan.santos53@etec.sp.gov.br>
2023-08-04 11:59:21 -0300

    finalizando tela de ajuda

--- LibellaApp/pages/home/DrawerPages/Ajuda.js
+++ LibellaApp/pages/home/DrawerPages/Ajuda.js
@@ -6,8 +6,11 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 const AjudaPage = () => {
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor={"white"} style="auto" />
-      <Text style={styles.font}>Como podemos ajudar?</Text>
+      <Text style={{
+        color: "#6D45C2",
+        fontSize: 24,
+        fontFamily: 'Comfortaa_500Medium',
+      }}>Como podemos ajudar?</Text>
       <View style={styles.inputbox}>
         <View style={styles.inputView}>
           <TextInput
@@ -19,9 +22,45 @@ const AjudaPage = () => {
       <View style={styles.containerTopics}>
         <Text style={{
           color: "black",
-          fontSize: 15
+          fontSize: 20,
+          fontWeight: 700,
         }}>Tópicos</Text>
-         <View style={styles.containerTopics2}></View>
+        <View style={styles.containerTopics2}>
+          <View style={styles.containerTopics3}></View>
+          <View style={styles.containerTopics3}></View>
+          <View style={styles.containerTopics3}></View>
+          <View style={styles.containerTopics3}></View>
+        </View>
+      </View>
+      <View style={styles.containerFale}>
+        <Text style={{
+          color: "#313131",
+          fontSize: 16,
+          fontWeight: 700,
+        }}>Não achou o que queria?<Text style={{
+          color: "#6D45C2",
+          fontSize: 16,
+          fontWeight: 700,
+        }}> Fale Conosco!</Text></Text>
+        <View style={{
+          width: '100%',
+          padding: 10,
+          borderRadius: 0,
+          backgroundColor: 'red',
+          backgroundColor: "white",
+          borderRadius: 10,
+          padding: 5,
+        }}>
+          <Text style={{
+            color: "#313131",
+            
+            textAlign: 'center',
+            fontSize: 14,
+            fontWeight: 700,
+            
+            
+          }}>libella@gmail.com</Text>
+        </View>
       </View>
     </View>
 
@@ -36,42 +75,53 @@ const styles = StyleSheet.create({
     backgroundColor: "#F2F2F2",
     alignItems: "center",
     justifyContent: "flex-start",
-    gap: 45,
-    top: 20,
+    gap: 36,
     paddingHorizontal: 30,
+    marginTop: 40,
   },
   containerTopics: {
     width: '100%',
-    height: '35%',
-    backgroundColor: 'red',
+    height: '45%',
+    backgroundColor: '#F2F2F2',
     gap: 10,
-    padding: 20,
+
     borderRadius: 10,
   },
   containerTopics2: {
     width: '100%',
-    height: '55%',
-    backgroundColor: 'blue',
-    gap: 10,
-    padding: 20,
+    height: '75%',
+    gap: 0,
     borderRadius: 10,
   },
-  font: {
-    color: "#6D45C2",
-    fontSize: 20,
+  containerTopics3: {
+    width: '100%',
+    height: '25%',
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 0,
+    borderBottomWidth: 0.5,
+  },
+  containerFale: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    width: '100%',
+    gap: 10,
+    backgroundColor: "#F2F2F2",
   },
   inputbox: {
     justifyContent: "center",
     gap: 27,
+    width: "100%",
   },
   input: {
     fontFamily: 'Poppins_300Light',
     fontSize: 16,
+    
     flex: 1,
   },
   inputView: {
-    height: 56,
-    width: 310,
+
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
