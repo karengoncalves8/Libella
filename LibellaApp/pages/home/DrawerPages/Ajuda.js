40045429e66d724eb142b48668c5438d24f5e7fa
ruanpreboucas <ruan.santos53@etec.sp.gov.br>
2023-08-06 12:57:49 -0300

    tela de ajuda finalizada

--- LibellaApp/pages/home/DrawerPages/Ajuda.js
+++ LibellaApp/pages/home/DrawerPages/Ajuda.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, TextInput, View, TouchableOpacity, Image } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import FeatherIcon from 'react-native-vector-icons/Feather'
 
 const AjudaPage = () => {
   return (
@@ -13,10 +14,15 @@ const AjudaPage = () => {
       }}>Como podemos ajudar?</Text>
       <View style={styles.inputbox}>
         <View style={styles.inputView}>
-          <TextInput
-            style={styles.input}
-            placeholder="Buscar"
+          <FeatherIcon
+            name="search"
+            size={25}
+            color={"#00000040"}
+            style={styles.icon}
           />
+          <TextInput style={styles.input}
+            placeholder="Buscar"
+            ></TextInput>
         </View>
       </View>
       <View style={styles.containerTopics}>
@@ -26,10 +32,18 @@ const AjudaPage = () => {
           fontWeight: 700,
         }}>Tópicos</Text>
         <View style={styles.containerTopics2}>
-          <View style={styles.containerTopics3}></View>
-          <View style={styles.containerTopics3}></View>
-          <View style={styles.containerTopics3}></View>
-          <View style={styles.containerTopics3}></View>
+          <View style={styles.containerTopics3}>
+            <Text style={styles.font} > Cadastrar Paciente</Text>
+          </View>
+          <View style={styles.containerTopics3}>
+            <Text style={styles.font}> Atribuir Atividade</Text>
+          </View>
+          <View style={styles.containerTopics3}>
+            <Text style={styles.font}> Alterar Senha</Text>
+          </View>
+          <View style={styles.containerTopics3}>
+            <Text style={styles.font}> Bloquear Notificações</Text>
+          </View>
         </View>
       </View>
       <View style={styles.containerFale}>
@@ -53,12 +67,9 @@ const AjudaPage = () => {
         }}>
           <Text style={{
             color: "#313131",
-            
             textAlign: 'center',
             fontSize: 14,
             fontWeight: 700,
-            
-            
           }}>libella@gmail.com</Text>
         </View>
       </View>
@@ -79,12 +90,31 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     marginTop: 40,
   },
+  inputbox: {
+    justifyContent: "center",
+    gap: 27,
+    width: "100%",
+  },
+  input: {
+    fontFamily: 'Poppins_300Light',
+    color:"red",
+    fontSize: 16,
+    marginLeft:8,
+    flex: 1,
+  },
+  inputView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    paddingHorizontal: 30,
+    backgroundColor: '#F8F8F8',
+  },
   containerTopics: {
     width: '100%',
-    height: '45%',
+    height: '46%',
     backgroundColor: '#F2F2F2',
     gap: 10,
-
     borderRadius: 10,
   },
   containerTopics2: {
@@ -97,9 +127,9 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '25%',
     backgroundColor: 'white',
-    padding: 20,
-    borderRadius: 0,
+    padding: 0,
     borderBottomWidth: 0.5,
+    justifyContent: 'center',
   },
   containerFale: {
     justifyContent: 'center',
@@ -109,26 +139,13 @@ const styles = StyleSheet.create({
     gap: 10,
     backgroundColor: "#F2F2F2",
   },
-  inputbox: {
-    justifyContent: "center",
-    gap: 27,
-    width: "100%",
-  },
-  input: {
+  font: {
+    color: "#313131",
+    fontSize: 14,
+    fontWeight: 500,
     fontFamily: 'Poppins_300Light',
-    fontSize: 16,
-    
-    flex: 1,
-  },
-  inputView: {
-
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    borderRadius: 30,
-    paddingHorizontal: 30,
-    backgroundColor: '#F8F8F8',
-  },
+    marginLeft: 5,
+  }
 
 
 });
