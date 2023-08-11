edcbfd28b26acf0d51c619a206c659a24094dd3d
ruanpreboucas <ruan.santos53@etec.sp.gov.br>
2023-08-11 11:56:50 -0300

    Tela precisa de uma API para a dashboard

--- LibellaApp/pages/home/Progresso.js
+++ LibellaApp/pages/home/Progresso.js
@@ -1,35 +1,53 @@
 import * as React from 'react';
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, TextInput, View, TouchableOpacity, Image } from "react-native";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import FeatherIcon from 'react-native-vector-icons/Feather'
 
-const ProgressoPage =({ navigation }) =>  {
+import Feather from "react-native-vector-icons/Feather";
+
+const ProgressoPage = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <View style={{
         width: '120%',
         height: '9%',
         backgroundColor: '#53A7D7',
-        justifyContent:'center',
+        justifyContent: 'center',
       }}><Text style={{
-        fontWeight:700,
-        left:50,
-        fontSize:24,
-        color:"white",
+        fontWeight: 700,
+        left: 50,
+        fontSize: 24,
+        color: "white",
         fontFamily: 'Poppins_300Light',
       }}>Progresso</Text>
       </View>
-        <View style={{
-          height:"12%",
-          width:"100%",
-          backgroundColor:"yellow",
-        }}>
-        
+      <View style={{
+        height: "10%",
+        width: "100%",
+        backgroundColor: "#F2F2F2",
+        flexDirection: 'row',
+        alignItems: 'center'
+      }}>
+        <Image
+          style={styles.userImg}
+          source={require('../../assets/img/Pessoas/Andreia.jpg')}
+        />
+        <Text style={{
+          fontWeight: 500,
+          left: 30,
+          fontSize: 17,
+          color: "#313131",
+          fontFamily: 'Poppins_500Medium',
+        }}>Andreia Ramos</Text>
       </View>
       <View style={styles.containerGraph}>
 
       </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Home")}
+        style={styles.pacienteContainer}>
+        <Feather name='home' size={25} color={'#53A7D7'} />
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -39,30 +57,31 @@ export default ProgressoPage;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
+    backgroundColor: "#F2F2F2",
     alignItems: "center",
     justifyContent: "flex-start",
-    marginTop: 40,
-    gap:40,
-    paddingHorizontal:30
+    marginTop: 25,
+    gap: 38,
+    paddingHorizontal: 30
   },
   container3: {
     flex: 1,
     backgroundColor: "#F2F2F2",
     alignItems: "center",
     justifyContent: "flex-start",
-    height:"100%",
-    width:"100%",
-    paddingHorizontal:30,
+    height: "100%",
+    width: "100%",
+    paddingHorizontal: 30,
   },
   containerGraph: {
     backgroundColor: "#F2F2F2",
     alignItems: "center",
     justifyContent: "flex-start",
-    height:"60%",
-    width:"100%",
-    paddingHorizontal:30,
-  
+    height: "60%",
+    width: "100%",
+    paddingHorizontal: 30,
+    borderWidth: 0.5
+
   },
   inputbox: {
     justifyContent: "center",
@@ -71,9 +90,9 @@ const styles = StyleSheet.create({
   },
   input: {
     fontFamily: 'Poppins_300Light',
-    color:"red",
+    color: "red",
     fontSize: 16,
-    marginLeft:8,
+    marginLeft: 8,
     flex: 1,
   },
   inputView: {
@@ -84,42 +103,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     backgroundColor: '#F8F8F8',
   },
-  containerTopics: {
-    width: '100%',
-    height: '46%',
-    backgroundColor: '#F2F2F2',
-    gap: 10,
-    borderRadius: 10,
-  },
-  containerTopics2: {
-    width: '100%',
-    height: '75%',
-    gap: 0,
-    borderRadius: 10,
-  },
-  containerTopics3: {
-    width: '100%',
-    height: '25%',
-    backgroundColor: 'white',
-    padding: 0,
-    borderBottomWidth: 0.5,
-    justifyContent: 'center',
-  },
-  containerFale: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    width: '100%',
-    gap: 10,
-    backgroundColor: "#F2F2F2",
-  },
   font: {
     color: "#313131",
     fontSize: 14,
     fontWeight: 500,
     fontFamily: 'Poppins_300Light',
     marginLeft: 5,
-  }
+  },
+  userImg: {
+    left: 10,
+    width: 70,
+    height: "100%",
+    borderRadius: 100,
+  },
 
 
 });
